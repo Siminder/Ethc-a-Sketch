414d384ba5d2526ef8c7086bf0c9bf812a9a46ee
Siminder Bansal <bansalsiminder234@gmail.com>
2026-02-21 22:44:53 +0000

    addded resizing function also added bonus that the div arent the smae color and played with opacitty to make them seem faded

--- Ethc-a-Sketch.js
+++ Ethc-a-Sketch.js
@@ -1,4 +1,4 @@
-    
+
 
 
 
@@ -6,10 +6,12 @@ let createDiv = (size) => {
     const container = document.querySelector("#container");
     const boxSize = Math.floor(800 / size); 
 
+    container.innerHTML = "";// refresh current html 
+
     const boxesPerRow = Math.floor(800 / boxSize);  // How many boxes per row
     const boxesPerColumn = Math.floor(800 / boxSize); // how many boxers per column
 
-
+   
 
     for(let d = 0; d < boxesPerRow * boxesPerColumn; d++ ){
         const box = document.createElement("div")
@@ -17,11 +19,18 @@ let createDiv = (size) => {
          box.style.width =`${boxSize}px`;
         box.style.height =`${boxSize}px`;
 
+    let R= Math.floor(Math.random() * 256)
+    
+    let B = Math.floor(Math.random() * 256)
+
+    let G = Math.floor(Math.random() * 256)
+    let randomColor ="rgb("+ R + "," + B + "," + G + ")";
 
        
    
          box.addEventListener("mouseover", () =>{
-            box.style.background = "blue"; 
+            box.style.backgroundColor = randomColor;
+            box.style.opacity = 0.33;
             
         })
 
@@ -34,11 +43,23 @@ let createDiv = (size) => {
     
 }
 
-    
+let resize = () => {
+    const button = document.querySelector("button");
 
-   
 
+    button.addEventListener("click", () =>{
+         size = Number(prompt("please enter a number 1-100"))
+        
+         createDiv(size)
+    })
+}
 
 
 
 createDiv(16);
+
+
+resize();
+
+    
+
