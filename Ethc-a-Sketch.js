bf02c99cae52665dec80d68ef55038ad9f51895c
Siminder Bansal <bansalsiminder234@gmail.com>
2026-02-22 03:03:41 +0000

    added final css to make everything look better, added final comments in code, and added some stuff to my read me file

--- Ethc-a-Sketch.js
+++ Ethc-a-Sketch.js
@@ -1,39 +1,54 @@
 
 
 
-
+// function to make a div and append it;
 let createDiv = (size) => {
-    const container = document.querySelector("#container");
+
+    // adding my container 
+    const container = document.querySelector("#container"); 
+    // size for box to fit in container nicely 
     const boxSize = Math.floor(800 / size); 
+    // refresh current html 
+    container.innerHTML = "";
 
-    container.innerHTML = "";// refresh current html 
-
-    const boxesPerRow = Math.floor(800 / boxSize);  // How many boxes per row
-    const boxesPerColumn = Math.floor(800 / boxSize); // how many boxers per column
+    // How many boxes per row
+    const boxesPerRow = Math.floor(800 / boxSize); 
+    
+    // how many boxers per column
+    const boxesPerColumn = Math.floor(800 / boxSize); 
 
    
-
+    //for loop to loop making as many boxes i need ie 16*16 = 256 boxes
     for(let d = 0; d < boxesPerRow * boxesPerColumn; d++ ){
+        //creating div element 
         const box = document.createElement("div")
+
+        //adding a class called box to it
         box.classList.add("box")
-         box.style.width =`${boxSize}px`;
+
+        // styling width and height of the divs
+        box.style.width =`${boxSize}px`;
         box.style.height =`${boxSize}px`;
 
-    let R= Math.floor(Math.random() * 256)
-    
-    let B = Math.floor(Math.random() * 256)
 
-    let G = Math.floor(Math.random() * 256)
-    let randomColor ="rgb("+ R + "," + B + "," + G + ")";
+        // RGB random number so it makes a random rgb number
+        let R= Math.floor(Math.random() * 256)
+        
+        let B = Math.floor(Math.random() * 256)
+
+        let G = Math.floor(Math.random() * 256)
+        let randomColor ="rgb("+ R + "," + B + "," + G + ")";
 
        
-   
+        //event listener that on mouse over div element so it changes color 
          box.addEventListener("mouseover", () =>{
+            //styles for backgorund color on mouse over
             box.style.backgroundColor = randomColor;
             box.style.opacity = 0.33;
             
         })
 
+        
          container.appendChild(box);
 
     }
@@ -43,22 +58,40 @@ let createDiv = (size) => {
     
 }
 
+//fucntion for button to resize 
 let resize = () => {
+    // adding button 
     const button = document.querySelector("button");
 
-
+    //event listener to button that when clicked
     button.addEventListener("click", () =>{
-         size = Number(prompt("please enter a number 1-100"))
+        //prompt that only take a number
+        size = Number(prompt("please enter a number 1-100"));
+
+        // checks if size is in 1-100  else alert that a invaild input
+        if(size > 1 && size < 100){
+            //pass size to funciton 
+            createDiv(size);
+         } else{
+            alert("Invaild input");
+
+         }
+
+         //checks for not a number and null
+         if(isNaN(size) || size === null){
+            alert("Invaild input");
+            
+         }
         
-         createDiv(size)
+        
     })
 }
 
 
-
+// starts on a 16*16 grid
 createDiv(16);
 
-
+// starts the button
 resize();
 
     
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Ethc-a-Sketch
+# This project is a demestoration of DOM and javascrtipt usage
+# I started with making a fucntion that loop a random size to make that many div and populate my container 
+# after adding my css and using flex-wrap and flexbox to help me wrap them around in my container 
+# I also added a button that has a click event listener 
+# this button had a function that check for click and let you resize the grid to any layout
+# I use math floor to divde the size by what my continer size to make sure they fix
+# I also added to diffrente variable that do the same thign in case i want to change the size of my continer in the future 
+# as a bonus i added a random number input for my RGB value when the mouse over on the div come i also changed the opacity to make it look more pastel like
+
+
+# Thank you for stopping by 
+# by Siminder Bansal
